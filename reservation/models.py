19f3cb6781c4b562de3f8dd022f68a72ee4b263a
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2023-03-23 14:53:04 +0800

    udpate tables

--- reservation/models.py
+++ reservation/models.py
@@ -3,6 +3,28 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+
+class UserDetails(models.Model):
+    GENDER = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    address = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=50, null=True)
+    bdate = models.DateField()
+    placebirth = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.middle_name} {self.last_name}'
+
+
 class Services(models.Model):
     service_name = models.CharField(max_length=255)
     service_description = models.TextField()
@@ -48,3 +70,29 @@ class ReservationFacilities(models.Model):
 
 
 # Admin/Doctor
+class Speciality(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class DoctorDetails(models.Model):
+    GENDER = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    address = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=50, null=True)
+    bdate = models.DateField()
+    placebirth = models.CharField(max_length=50, null=True)
+    speciality = models.ManyToManyField(Speciality)
+
+    def __str__(self):
+        return f'{self.first_name} {self.middle_name} {self.last_name}'
